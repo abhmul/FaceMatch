8c935db19fe9a63c6bdb91b8a9591976707ca13c
abhmul <abhmul@gmail.com>
2016-04-10 16:41:32 -0500

    added mysql connection

--- web/config/connection.php
+++ web/config/connection.php
@@ -8,7 +8,7 @@
 
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', 'napasbang');
-DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_HOST', 'mysql://bc8d51af1970c7:d9b58a52@us-cdbr-iron-east-03.cleardb.net/heroku_22bf2fadbb5295c?reconnect=true');
 DEFINE ('DB_NAME', 'faceMatch_survey');
 
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ("Connection failed: " . mysqli_connect_error() );
