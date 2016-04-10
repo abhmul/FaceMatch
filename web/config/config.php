daf002d79bd0ffd4fa2b499770124106f5373a2e
abhmul <abhmul@gmail.com>
2016-04-10 16:35:49 -0500

    built template.php

--- web/config/config.php
+++ web/config/config.php
@@ -6,9 +6,9 @@
  * Time: 3:09 PM
  */
 
-include('config/connection.php'); //Attach our connection script
+include('connection.php'); //Attach our connection script
 
-include('functions/template.php'); //Attach our template functions
+include('../functions/template.php'); //Attach our template functions
 //include('functions/sandbox.php') //Attach our unsorted functions
 
 
