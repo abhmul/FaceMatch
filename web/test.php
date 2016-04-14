ad9a62927daff6d9ce499ede9ca2337bbbf2e8df
abhmul <abhmul@gmail.com>
2016-04-13 21:28:13 -0500

    Trying to debug SQL problems

--- web/test.php
+++ web/test.php
@@ -3,6 +3,21 @@
 <body>
 
 <?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 echo "Does it work?";
 
 $a = $_POST["email"];
