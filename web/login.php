72843def904fbf6677cef8f969341a25ce6b3561
abhmul <abhmul@gmail.com>
2016-04-16 16:59:47 -0500

    Changed some font sizes

--- web/login.php
+++ web/login.php
@@ -17,7 +17,7 @@ include('config/connection.php'); //Attach our connection script
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <style type="text/css">
         h1 { font-family: Roboto; font-size: 49px; }
-        h2 { font-family: Roboto; font-size: 20px; }
+        h2 { font-family: Roboto; font-size: 30px; }
         td { text-align:center; }
     </style>
 </head>
@@ -26,11 +26,11 @@ include('config/connection.php'); //Attach our connection script
 <h1>FaceMatch Survey Login</h1>
 <h2>Log in to continue you the survey from where you left off.</h2>
 <div class="entry">
-    <h3 class="qTitle" style = "font: 15px roboto, sans-serif;">Username or NetID: </h3>
+    <h3 class="qTitle" style = "font: bold 20px roboto, sans-serif;">Username or NetID: </h3>
     <form action="index.php" method="post">
         <input type="text" name="email" size="20" style = "font: 30px roboto, sans-serif;"/>
-        <input type="submit" value="I like Guys!" name="like" />
-        <input type="submit" value="I like Girls!" name="like" />
+        <input type="submit" value="I like Guys!" name="like" style = "height: 33px; width: 60px; font: 25px roboto, sans-serif;" />
+        <input type="submit" value="I like Girls!" name="like" style = "height: 33px; width: 60px; font: 25px roboto, sans-serif;"/>
 <!--        <input type="hidden" name="questionid" value="questionid" />-->
 <!--        <input type="hidden" name="submitted" value="1" />-->
         <input type = 'hidden' name = 'logged_in?' value = "1">
