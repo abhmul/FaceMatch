8dad23473f9462f2bada567bc10bed1dfec5c698
abhmul <abhmul@gmail.com>
2016-04-17 15:37:36 -0500

    put logout on right side

--- web/index.php
+++ web/index.php
@@ -184,7 +184,7 @@ else {
 <div class="wrapOverall" style="height: 100%">
     <div class="header">
         <h1 class='siteTitle' style = "font: 49px roboto, sans-serif;">FaceMatch Survey</h1>
-        <a class = 'siteTitle' href="login.php" style = "text-align: right;">Logout</a>
+        <a class = 'siteTitle' href="login.php"><p align = "right">Logout</p></a>
     </div><!-- END header -->
     <div class="wrapContent" style="height: 100%">
         <div class="content">
